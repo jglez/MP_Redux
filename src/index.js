af84718588fc6740766e2f7b74781dcb4a08adab
jglez <jglez.cs@gmail.com>
2021-02-16 23:38:23 -0600

    Added react-redux and redux dependency, created store obj and reducer

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Import createStore function from Redux
+import { createStore } from 'redux'
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
+
+// We need a Reducer function to pass into our createStore()
+const reducer = () => {
+  return {
+    name: 'elliot',
+    faction: 'fsociety'
+  }
+}
+
+const store = createStore(reducer)
+
+console.log(store)
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
